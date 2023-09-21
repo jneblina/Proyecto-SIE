2a84101dc86e35cd2de963f4b67027eab836c5e0
jneblina <janneblina7@gmail.com>
2023-09-21 16:13:08 -0700

    Actualizacion header

--- sie-rework/components/Header.jsx
+++ sie-rework/components/Header.jsx
@@ -1,7 +1,9 @@
 export default function Header() {
     return (
-        <header className="bg-green-950 w-full h-[20%] p-2 text-white">
-            Sistema de Integracion Escolar
+        <header className="bg-green-950 w-full h-[20%] p-2 text-white text-center">
+            <div className="flex flex-col">
+                SIE
+            </div>
 
         </header>
     )
