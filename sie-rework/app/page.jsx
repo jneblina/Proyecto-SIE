4c307733c723ee0a472f67869ad9588557319a56
Hugo Corona <hugo.corona.r@gmail.com>
2023-09-30 20:49:03 -0700

    mejora de la navbar

--- sie-rework/app/page.jsx
+++ sie-rework/app/page.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import LoginForm from "@/components/LoginForm";
@@ -9,7 +9,6 @@ export default function Home() {
       <Header />
       <main className="bg-[#F5F0EA] flex w-screen h-screen items-start py-12 justify-center">
         <LoginForm />
-        
       </main>
       <Footer />
     </>
