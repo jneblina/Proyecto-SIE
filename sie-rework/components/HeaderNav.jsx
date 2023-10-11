b969e7783722827f73800bacdab57a4e41649ba9
Hugo Corona <hugo.corona.r@gmail.com>
2023-10-10 21:28:13 -0700

    control de refresco de sesión

--- sie-rework/components/HeaderNav.jsx
+++ sie-rework/components/HeaderNav.jsx
@@ -87,26 +87,6 @@ const HeaderNav = () => {
       <Navbar maxWidth="2xl" height={"3rem"} className="bg-zinc-900">
         <NavbarContent>
           <NavbarBrand>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              color="white"
-              class="icon icon-tabler icon-tabler-brand-electronic-arts"
-              width="38"
-              height="38"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
-              <path d="M17.5 15l-3 -6l-3 6h-5l1.5 -3"></path>
-              <path d="M17 14h-2"></path>
-              <path d="M6.5 12h3.5"></path>
-              <path d="M8 9h3"></path>
-            </svg>
             <p className="font-bold text-white ml-1">
               SISTEMA DE INTEGRACIÓN ESCOLAR
             </p>
